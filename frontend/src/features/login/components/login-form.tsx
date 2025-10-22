a37dfbd46d36573aee26333dacb4cfb48a17e840
Solomon Nwante <solomonchuks202@gmail.com>
2025-10-22 15:15:56 +0100

    refactor: rename and reorganize auth components under login module
    - Renamed auth-form → login-form
    - Renamed auth-image → login-image
    - Renamed auth-welcome → login-welcome
    - Moved files from auth/ to login/
    - Updated imports and layout paths accordingly

--- frontend/src/features/login/components/login-form.tsx
+++ frontend/src/features/login/components/login-form.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Box from '@/components/ui/box';
+import { getters } from '@/lib/config/i18n';
 import Link from 'next/link';
 
-const AuthForm: React.FC = () => {
+const LoginForm: React.FC = () => {
+  const lang = 'en';
+  const text = getters.geti18ns()[lang].login.form;
+
   return (
     <Box as="form" className="w-full max-w-md" onSubmit={e => e.preventDefault()}>
       <Box as="p" className="mb-4 text-[#808080]">
-        Log in to your account
+        {text.title}
       </Box>
+
       <Box as="label" htmlFor="email" className="block text-sm text-[#605D64] mb-1">
-        Email Address
+        {text.email}
       </Box>
       <Box
         as="input"
@@ -19,11 +24,11 @@ const AuthForm: React.FC = () => {
         placeholder="you@example.com"
         required
         className="w-full p-3 rounded-md bg-white text-black mb-4"
-        aria-label="Email Address"
+        aria-label={text.email}
       />
 
       <Box as="label" htmlFor="password" className="block text-sm text-[#605D64] mb-1">
-        Password
+        {text.password}
       </Box>
       <Box
         as="input"
@@ -33,13 +38,13 @@ const AuthForm: React.FC = () => {
         placeholder="••••••••"
         required
         className="w-full p-3 rounded-md bg-white text-black mb-3"
-        aria-label="Password"
+        aria-label={text.password}
       />
 
       <Box className="flex items-center gap-2">
         <Box as="input" id="remember" type="checkbox" className="h-4 w-4" />
         <Box as="label" htmlFor="remember" className="text-sm text-[#605D64]">
-          Remember me
+          {text.remember}
         </Box>
       </Box>
 
@@ -49,19 +54,19 @@ const AuthForm: React.FC = () => {
           type="submit"
           className="w-full px-4 py-3 rounded-md bg-[#3b82f6] text-white font-medium"
         >
-          Log In
+          {text.login}
         </Box>
         <Box
           as={Link}
           href="/"
           className="text-sm text-gray-400 hover:text-gray-200 transition"
-          aria-label="Go to hpmepage"
+          aria-label={text.back}
         >
-          Back to homepage
+          {text.back}
         </Box>
       </Box>
     </Box>
   );
 };
 
-export { AuthForm };
+export { LoginForm };
--- frontend/src/features/login/components/login-welcome.tsx
+++ frontend/src/features/login/components/login-welcome.tsx
@@ -1,28 +1,26 @@
 import Box from '@/components/ui/box';
 import { Users } from '@/types/auth';
 import { useSearchParams } from 'next/navigation';
+import { getters } from '@/lib/config/i18n';
 
 const Welcome = () => {
   const searchParams = useSearchParams();
   const role = (searchParams.get('role') as Users) || 'student';
+  const lang = 'en';
 
-  const subtitle: Record<typeof role, string> = {
-    student:
-      'Access your courses, submit assignments, track your progress, and stay on top of your learning goals all in one place.',
-    mentor: 'Manage admissions, guide students, track attendance, and provide feedback seamlessly.',
-  };
+  const text = getters.geti18ns()[lang].login;
 
   return (
     <Box className="w-full max-w-md max-auto mb-8">
       <Box as="h1" className="text-4xl md:5xl font-semibold leading-tight text-white text-center">
-        Welcome!
+        {text.title}
       </Box>
 
       <Box
         as="p"
         className="mt-4 text-sm md:text-base text-gray-300 text-center max-w-[36rem] mx-auto"
       >
-        {subtitle[role]}
+        {text.subtitles[role]}
       </Box>
     </Box>
   );
