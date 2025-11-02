6e299744183c548dccbbb0ec9ebfe51f0121fd62
Solomon Nwante <solomonchuks202@gmail.com>
2025-11-02 19:37:05 +0100

    chore: remove unecessary comments

--- frontend/src/features/login/layout/login-layout.tsx
+++ frontend/src/features/login/layout/login-layout.tsx
@@ -8,11 +8,9 @@ const LoginLayout = () => {
   return (
     <Box className="min-h-screen bg-gray-900 flex items-center justify-center">
       <Box className="w-full max-w-5xl overflow-hidden bg-[url('/images/auth-bg.png')] bg-cover bg-no-repeat bg-[#0b0b0f] grid grid-cols-1 md:grid-cols-2 shadow-lg">
-        {/* Left - image section */}
         <LoginImage />
-        {/* Right - dark panel, form section */}
         <Box className="w-full max-w-md p-8 md:p-12 flex flex-col items-center justify-center mx-auto">
-          <Logo className="mb-4 md:mb-0 md:invisible rounded-sm" clickable={false} />
+          <Logo className="mb-4 md:mb-0 md:invisible rounded-sm" />
           <Welcome />
           <LoginForm />
         </Box>
